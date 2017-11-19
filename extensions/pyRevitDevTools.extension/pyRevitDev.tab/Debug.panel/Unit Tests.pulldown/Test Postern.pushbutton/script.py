96b87d0fb3c0513adb63bd0eff0d58a5857e010f
Ehsan Iran-Nejad <eirannejad@gmail.com>
2017-11-18 23:44:56 -0800

    Updated expectations of module interface

--- extensions/pyRevitDevTools.extension/pyRevitDev.tab/Debug.panel/Unit Tests.pulldown/Test Postern.pushbutton/script.py
+++ extensions/pyRevitDevTools.extension/pyRevitDev.tab/Debug.panel/Unit Tests.pulldown/Test Postern.pushbutton/script.py
@@ -5,22 +5,44 @@ from postern import Postern, request
 app = Postern('testapp')
 
 
-# @app.route('/select/<int:element_id>')
-# def select(uiapp, element_id):
-#     pass
+# POST /select/2413
+@app.route('/select/<int:element_id>', methods=['POST'])
+def select(uiapp, element_id):
+    pass
 
 
+# GET  /query?objtype=Wall
+@app.route('/query')
+def query(uiapp):
+    objtype = request.args.get('objtype')
+    if objtype == 'Wall':
+        walls_data = [{'objtype': 'Wall',
+                       'id': 100,
+                       'properties': ['prop1', 'prop2', 'prop3']
+                       }]
+
+        return walls_data
+
+
+# POST /testfunc/anotherlevel
+# { "message": "This is the TaskDialog message." }
+# GET  /testfunc/anotherlevel
 @app.route('/testfunc/anotherlevel', methods=['GET', 'POST'])
 def testfunc(uiapp):
     if request.method == 'POST':
         data = request.data
         UI.TaskDialog.Show('Postern App', data['message'])
     elif request.method == 'GET':
-        return {'somekey': 18,
-                'someotherkey': [1, 2, 3, 'reza']}
+        somedata = {'somevalue': 18,
+                    'somelist': [1, 2, 3, 'name'],
+                    'someobj': {1: 2,
+                                3: 4}
+                    }
+        return somedata
 
 
 app.run()
+
 
 from postern import rulemap
 print(rulemap.get_rule('testapp', '/testfunc/anotherlevel', 'GET'))
